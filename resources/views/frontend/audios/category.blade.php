575aac2fcacf91d15a660a893e018e3100baa379
Mohammed Jacksi <moh.jacksi@yahoo.com>
2022-05-28 14:51:58 +0300

    fix about %75 of bugs

--- resources/views/frontend/audios/category.blade.php
+++ resources/views/frontend/audios/category.blade.php
@@ -102,58 +102,61 @@
 
                                 <div class="articles-container side-box-style     categories-container">
 
-                                    <div class="duplicated-box box-side side-categories">
+                                    @if ($category->childCategories)
+                                        <div class="duplicated-box box-side side-categories">
 
-                                        <div class="box-header ">
+                                            <div class="box-header ">
 
-                                            <div class="header-title ">
+                                                <div class="header-title ">
 
-                                                الاقسام الفرعية
+                                                    الاقسام الفرعية
 
-                                            </div>
-
-
-                                        </div>
-                                        <div class="box-body box-padding">
-
-                                            <div
-                                                class="row row-cols-lg-1  row-cols-md-3 row-cols-sm-2  row-cols-1 justify-content-center align-items-start">
-                                                @foreach ($category->childCategories as $sub_category)
-                                                    <div class="category-wrap">
-
-                                                        <a class="category-link"
-                                                            href="{{ route('frontend.audios.category', $sub_category->id) }}">
-
-
-                                                            <div class="main-content">
-
-                                                                <span class="icon">
-
-                                                                    <svg xmlns="http://www.w3.org/2000/svg" width="19.272"
-                                                                        height="15" viewBox="0 0 19.272 15">
-                                                                        <path id="folder-open"
-                                                                            d="M4.949,37.357H16.071V35.75a1.608,1.608,0,0,0-1.607-1.607H9.107L6.964,32H1.607A1.607,1.607,0,0,0,0,33.607v11l3.031-6.064A2.135,2.135,0,0,1,4.949,37.357Zm13.235,1.071H4.949a1.067,1.067,0,0,0-.958.593L0,47H14.97a1.072,1.072,0,0,0,.958-.592l3.214-6.429A1.052,1.052,0,0,0,18.184,38.429Z"
-                                                                            transform="translate(0 -32)" />
-                                                                    </svg>
-
-
-                                                                </span>
-                                                                <span class="text">{{ $sub_category->name }}</span>
-
-
-                                                            </div>
-
-                                                        </a>
-
-                                                    </div>
-                                                @endforeach
+                                                </div>
 
 
                                             </div>
+                                            <div class="box-body box-padding">
+
+                                                <div
+                                                    class="row row-cols-lg-1  row-cols-md-3 row-cols-sm-2  row-cols-1 justify-content-center align-items-start">
+                                                    @foreach ($category->childCategories as $sub_category)
+                                                        <div class="category-wrap">
+
+                                                            <a class="category-link"
+                                                                href="{{ route('frontend.audios.category', $sub_category->id) }}">
+
+
+                                                                <div class="main-content">
+
+                                                                    <span class="icon">
+
+                                                                        <svg xmlns="http://www.w3.org/2000/svg" width="19.272"
+                                                                            height="15" viewBox="0 0 19.272 15">
+                                                                            <path id="folder-open"
+                                                                                d="M4.949,37.357H16.071V35.75a1.608,1.608,0,0,0-1.607-1.607H9.107L6.964,32H1.607A1.607,1.607,0,0,0,0,33.607v11l3.031-6.064A2.135,2.135,0,0,1,4.949,37.357Zm13.235,1.071H4.949a1.067,1.067,0,0,0-.958.593L0,47H14.97a1.072,1.072,0,0,0,.958-.592l3.214-6.429A1.052,1.052,0,0,0,18.184,38.429Z"
+                                                                                transform="translate(0 -32)" />
+                                                                        </svg>
+
+
+                                                                    </span>
+                                                                    <span
+                                                                        class="text">{{ $sub_category->name }}</span>
+
+
+                                                                </div>
+
+                                                            </a>
+
+                                                        </div>
+                                                    @endforeach
+
+
+                                                </div>
+
+                                            </div>
 
                                         </div>
-
-                                    </div>
+                                    @endif
 
                                 </div>
 
--- resources/views/frontend/blogs/category.blade.php
+++ resources/views/frontend/blogs/category.blade.php
@@ -1,5 +1,23 @@
 @extends('layouts.frontend')
-@section('page_title', 'المقالات')
+@section('page_title', $category->name)
+@section('breadcrumb')
+    <li class="breadcrumb-item"><a href="{{ route('frontend.audios.index') }}">المقالات</a></li>
+    @php
+    $parent = $category->parentCategory;
+    $up_parent = $parent?->parentCategory;
+    @endphp
+    @if ($up_parent)
+        <li class="breadcrumb-item"><a
+                href="{{ route('frontend.audios.category', $up_parent->id) }}">{{ $up_parent->name }}</a>
+        </li>
+    @endif
+    @if ($parent)
+        <li class="breadcrumb-item"><a
+                href="{{ route('frontend.audios.category', $parent->id) }}">{{ $parent->name }}</a>
+        </li>
+    @endif
+    </li>
+@endsection
 
 @section('content')
 
@@ -14,57 +32,63 @@
                 <div class="col-lg-4 half order-2 order-lg-1">
 
                     <div class="side-box-container">
+                        @dd($category->childCategories)
+                        @if ($category->childCategories)
 
-                        <div class="box-wrap">
+                            <div class="box-wrap">
 
-                            <div class="duplicated-box-wrapper categories-container ">
+                                <div class="duplicated-box-wrapper categories-container ">
 
-                                <div class="duplicated-box box-side side-categories  articles-categories-items single-items">
+                                    <div
+                                        class="duplicated-box box-side side-categories  articles-categories-items single-items">
 
-                                    <div class="box-header box-padding ">
+                                        <div class="box-header box-padding ">
 
-                                        <div class="header-title ">
+                                            <div class="header-title ">
 
-                                            الاقسام الفرعية
+                                                الاقسام الفرعية
+
+                                            </div>
+
 
                                         </div>
+                                        <div class="box-body box-padding">
+
+                                            <div
+                                                class="row row-cols-lg-1   row-cols-md-3 row-cols-sm-2  row-cols-1 justify-content-start align-items-stretch gx-3">
+
+                                                @foreach ($category->childCategories as $sub_category)
+                                                    <div class="category-wrap">
+
+                                                        <a class="category-link"
+                                                            href="{{ route('frontend.blogs.category', $sub_category->id) }}">
 
 
-                                    </div>
-                                    <div class="box-body box-padding">
+                                                            <div class="main-content">
 
-                                        <div
-                                            class="row row-cols-lg-1   row-cols-md-3 row-cols-sm-2  row-cols-1 justify-content-start align-items-stretch gx-3">
+                                                                <span class="icon">
 
-                                            @foreach ($category->childCategories as $sub_category)
-                                                <div class="category-wrap">
-
-                                                    <a class="category-link"
-                                                        href="{{ route('frontend.blogs.category', $sub_category->id) }}">
-
-
-                                                        <div class="main-content">
-
-                                                            <span class="icon">
-
-                                                                <svg xmlns="http://www.w3.org/2000/svg" width="19.272"
-                                                                    height="15" viewBox="0 0 19.272 15">
-                                                                    <path id="folder-open"
-                                                                        d="M4.949,37.357H16.071V35.75a1.608,1.608,0,0,0-1.607-1.607H9.107L6.964,32H1.607A1.607,1.607,0,0,0,0,33.607v11l3.031-6.064A2.135,2.135,0,0,1,4.949,37.357Zm13.235,1.071H4.949a1.067,1.067,0,0,0-.958.593L0,47H14.97a1.072,1.072,0,0,0,.958-.592l3.214-6.429A1.052,1.052,0,0,0,18.184,38.429Z"
-                                                                        transform="translate(0 -32)" />
-                                                                </svg>
+                                                                    <svg xmlns="http://www.w3.org/2000/svg" width="19.272"
+                                                                        height="15" viewBox="0 0 19.272 15">
+                                                                        <path id="folder-open"
+                                                                            d="M4.949,37.357H16.071V35.75a1.608,1.608,0,0,0-1.607-1.607H9.107L6.964,32H1.607A1.607,1.607,0,0,0,0,33.607v11l3.031-6.064A2.135,2.135,0,0,1,4.949,37.357Zm13.235,1.071H4.949a1.067,1.067,0,0,0-.958.593L0,47H14.97a1.072,1.072,0,0,0,.958-.592l3.214-6.429A1.052,1.052,0,0,0,18.184,38.429Z"
+                                                                            transform="translate(0 -32)" />
+                                                                    </svg>
 
 
-                                                            </span>
-                                                            <span class="text">{{ $sub_category->name }}</span>
+                                                                </span>
+                                                                <span
+                                                                    class="text">{{ $sub_category->name }}</span>
 
 
-                                                        </div>
+                                                            </div>
 
-                                                    </a>
+                                                        </a>
 
-                                                </div>
-                                            @endforeach
+                                                    </div>
+                                                @endforeach
+
+                                            </div>
 
                                         </div>
 
@@ -74,9 +98,7 @@
 
 
                             </div>
-
-
-                        </div>
+                        @endif
 
                         <div class="box-wrap">
 
