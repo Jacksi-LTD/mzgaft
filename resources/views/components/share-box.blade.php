a5f386049b403cc03c0341149658628169b9903c
Noorallah <50310268+Noorallah20@users.noreply.github.com>
2023-04-09 21:57:34 +0300

    Update share-box.blade.php

--- resources/views/components/share-box.blade.php
+++ resources/views/components/share-box.blade.php
@@ -2,7 +2,7 @@
 
     <div class="share-items">
 
-        <span class="share-item share-text">شارك</span>
+        <span class="share-item share-text">پارڤە بکە</span>
         <a class="share-item share-link" href="#">
 
             <svg xmlns="http://www.w3.org/2000/svg" width="21.333" height="15" viewBox="0 0 21.333 15">
